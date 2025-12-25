d5ec0532b0a90f8cdaee543696aa8734fb74f742
prem22k <premsai224k@gmail.com>
2025-12-26 00:38:13 +0530

    chore: remove unused files and boilerplate

--- api/simple.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({ 
-    message: 'Simple JS API working!',
-    method: req.method,
-    timestamp: new Date().toISOString()
-  });
-}
